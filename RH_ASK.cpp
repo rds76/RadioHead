5e52ee9dba743ce5487af47683fe099b3d6c1624
Radim <admin@radas.net>
2024-11-29 22:33:11 +0100

    undo

--- RH_ASK.cpp
+++ RH_ASK.cpp
@@ -525,9 +525,10 @@ void RH_ASK::timerSetup()
 //    timer0_write(ESP.getCycleCount() + 41660000);
 #elif (RH_PLATFORM == RH_PLATFORM_ESP32)
     void RH_INTERRUPT_ATTR esp32_timer_interrupt_handler(); // Forward declaration
-    timer = timerBegin(1000000);
-    timerAttachInterrupt(timer, &esp32_timer_interrupt_handler);
-    timerAlarm(timer, 1000000 / _speed / 8, true, 0);
+    timer = timerBegin(0, 80, true); // Alarm value will be in in us
+    timerAttachInterrupt(timer, &esp32_timer_interrupt_handler, true);
+    timerAlarmWrite(timer, 1000000 / _speed / 8, true);
+    timerAlarmEnable(timer);
 #endif
 
 }
